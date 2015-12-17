05449cf9189d5d871ec6c92fbb0a8f75ffe3f0f2
sandeeppal1991 <dragoondranzertigerclaw@gmail.com>
2015-12-17 16:13:19 +0530

    data added

--- Portfolio/Portfolio/Scripts/app.js
+++ Portfolio/Portfolio/Scripts/app.js
@@ -1,6 +1,9 @@
 ﻿var app = angular.module("portfolio", []);
 
-app.controller("portController", function ($scope) {
+app.controller("portController", function ($scope,$http) {
     $scope.firstName = "John";
     $scope.lastName = "Doe";
+    $http.get('/Scripts/data.json').success(function (data) {
+        $scope.works = data;
+    });
 });
